07789c0596284433bfb6ba831f1dd9056cbabcda
Luong T Quan <luongquan@Luongs-MBP.hsd1.wa.comcast.net>
2021-03-07 19:54:45 -0800

    new update

--- app_server.R
+++ app_server.R
@@ -1,1 +1,23 @@
 #server page
+#Load library
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(maps)
+library(plotly)
+
+#Load dataset for chart 2
+gdp_chart <- read.csv("https://raw.githubusercontent.com/tracytai1999/Exploratory-Analysis/main/Exploratory%20Analysis/GDP.csv")
+
+
+#Create a server for 3 charts
+server <- function(input, output) {
+#Chart 1
+  
+  
+#Chart 2
+
+
+#Chart 3
+}
--- app_ui.R
+++ app_ui.R
@@ -1,1 +1,36 @@
 # ui page
+#Load library
+library(tidyverse)
+library(shiny)
+library(countrycode)
+library(ggplot2)
+library(plotly)
+
+#Connect the server to UI
+source("app_server.R")
+
+#Introduction
+introduction_tab <- tabPanel("Introduction")
+
+#Chart 1
+chart1_tab <- tabPanel("Chart 1")
+
+#Chart 2
+chart2_tab <- tabPanel("Chart 2")
+
+#Chart 3
+chart3_tab <- tabPanel("Chart 3")
+
+#Conclusion
+conclution_tab <- tabPanel("Conclution")
+
+#Create a UI 
+ui <- navbarPage(
+  "Final Deliverible",
+  introduction_tab,
+  chart1_tab,
+  chart2_tab,
+  chart3_tab,
+  conclution_tab
+  
+)
