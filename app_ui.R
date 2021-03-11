44a74c6df772d92c9072d257fce66505d4036f44
Ayaan Mohamed <ayaanmohamed@Ayaans-MacBook-Air.local>
2021-03-10 20:53:38 -0800

    Finish the intro and conclusion

--- app_ui.R
+++ app_ui.R
@@ -12,7 +12,21 @@ source("app_server.R")
 #Introduction
 introduction_tab <- tabPanel(
   "Introduction",
-  p("This is an introduction of our project")
+  p("This is an introduction of our project"),
+  p(" As one of the biggest pandemics entered the world named Covid-19, everyone 
+    has been affected by this in someway. Whether it be their health, their 
+    families or their jobs. This pandemic has caused people to work from their
+    home to stay safe and healthy becuase Covid-19 is very quick and contagious.
+    resturants, malls, and movies have been closed for this cause, and a lot
+    of businesses have been affected. In this project, we want to use data
+    visualiaztion to show how covid 10 has effected the economy in each state,
+    and compare data from each state to see what actions and policies are useful
+    to solve the problem."),
+  h2("Research Quesions"),
+  p(" What is the increasing trend for covid cases in each state?"),
+  p(" Do any of the states have increasing or decreasing cases due to Covid?"),
+  p(" How has Covid affected the people that are not diagnosed with Covid")
+  
                              
 )
 
@@ -84,7 +98,13 @@ chart3_tab <- tabPanel("Chart 3")
 #Conclusion
 conclusion_tab <- tabPanel(
   "Data Analysis",
-  p("This is an summary takeout for our project.")
+  p(" In conclusion,from the graphs we designed we were able to distinguish
+    the differences and simlarities between states. Whether the rates are high
+    or low. We made charts for the states that impacted covid, and showed 
+    how high it rose. For example in the chart for Alaska, between January 2020
+    to Jancuary 2021, the rates rose pretty quickly and it stays consistently
+    growing. We used data visualization to compare the states and show whether 
+    the cases increased or decreased and how it impacted people. ")
 
   )
 
